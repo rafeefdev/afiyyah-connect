dd71b912458f99b675aa8b70b4eb8bf54eb2db6b
rafeefdev <rafeef.dev03@gmail.com>
2025-06-20 10:48:58 +0700

    init folder structure

---
+++ lib/core/model/santri.dart
@@ -0,0 +1,9 @@
+import 'package:afiyyah_connect/core/model/hujroh.dart';
+
+class Santri {
+  String name;
+  DateTime tahunMasuk;
+  Hujroh hujroh;
+
+  Santri({required this.name, required this.tahunMasuk, required this.hujroh});
+}
