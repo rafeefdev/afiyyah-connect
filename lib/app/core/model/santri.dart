2c9d1f16b6a6723f88d328622400f210a0a68531
rafeefdev <rafeef.dev03@gmail.com>
2025-07-01 08:43:45 +0700

    add 'kelas' class and add it as a santri property

--- lib/app/core/model/santri.dart
+++ lib/app/core/model/santri.dart
@@ -1,9 +1,16 @@
 import 'package:afiyyah_connect/app/core/model/hujroh.dart';
+import 'package:afiyyah_connect/app/core/model/kelas.dart';
 
 class Santri {
   String name;
   DateTime tahunMasuk;
   Hujroh hujroh;
+  Kelas kelas;
 
-  Santri({required this.name, required this.tahunMasuk, required this.hujroh});
+  Santri({
+    required this.name,
+    required this.tahunMasuk,
+    required this.hujroh,
+    required this.kelas,
+  });
 }
