47cba76df3773715a6d3fbc8ced498ddf5ae48bc
rafeefdev <rafeef.dev03@gmail.com>
2025-07-02 11:00:36 +0700

    adding google_font package and make typography class

--- lib/app/themes/app_themedata.dart
+++ lib/app/themes/app_themedata.dart
@@ -1,3 +1,4 @@
+import 'package:afiyyah_connect/app/themes/app_typography.dart';
 import 'package:flutter/material.dart';
 
-ThemeData appTheme = ThemeData();
+ThemeData appTheme = ThemeData(textTheme: AppTypography.plusJakartaTextTheme);
