aa251929bd8ed6fae02197fa5721774477427a62
rafeefdev <rafeef.dev03@gmail.com>
2025-07-06 13:28:58 +0700

    add code for app entities and logics

---
+++ lib/app/core/model/entities/santri.dart
@@ -0,0 +1,15 @@
+class Santri {
+  String id;
+  String name;
+  DateTime tahunMasuk;
+  String hujrohId;
+  String kelasId;
+
+  Santri({
+    required this.id,
+    required this.name,
+    required this.tahunMasuk,
+    required this.hujrohId,
+    required this.kelasId,
+  });
+}
