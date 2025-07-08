8ccdf49adbe784a71f590e501bb4f72c8bd5b8b3
rafeefdev <rafeef.dev03@gmail.com>
2025-07-08 13:08:02 +0700

    pendataan kesehatan state

---
+++ lib/features/health_input/viewmodel/pendataan_kesehatan_provider.dart
@@ -0,0 +1,89 @@
+import 'package:afiyyah_connect/app/core/model/entities/santri.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'pendataan_kesehatan_provider.g.dart';
+
+enum PeriksaKlinikStatus {
+  sudah,
+  belum,
+  luar,
+  none,
+}
+
+class PendataanKesehatanState {
+  final Santri? santri;
+  final List<String> keluhan;
+  final DateTime? sickStartTime;
+  final PeriksaKlinikStatus periksaKlinikStatus;
+  final bool isSubmitting;
+
+  PendataanKesehatanState({
+    this.santri,
+    this.keluhan = const [],
+    this.sickStartTime,
+    this.periksaKlinikStatus = PeriksaKlinikStatus.none,
+    this.isSubmitting = false,
+  });
+
+  PendataanKesehatanState copyWith({
+    Santri? santri,
+    List<String>? keluhan,
+    DateTime? sickStartTime,
+    PeriksaKlinikStatus? periksaKlinikStatus,
+    bool? isSubmitting,
+  }) {
+    return PendataanKesehatanState(
+      santri: santri ?? this.santri,
+      keluhan: keluhan ?? this.keluhan,
+      sickStartTime: sickStartTime ?? this.sickStartTime,
+      periksaKlinikStatus: periksaKlinikStatus ?? this.periksaKlinikStatus,
+      isSubmitting: isSubmitting ?? this.isSubmitting,
+    );
+  }
+}
+
+@riverpod
+class PendataanKesehatan extends _$PendataanKesehatan {
+  @override
+  PendataanKesehatanState build() {
+    return PendataanKesehatanState();
+  }
+
+  void setSantri(Santri santri) {
+    state = state.copyWith(santri: santri);
+  }
+
+  void toggleKeluhan(String keluhan) {
+    final currentKeluhan = List<String>.from(state.keluhan);
+    if (currentKeluhan.contains(keluhan)) {
+      currentKeluhan.remove(keluhan);
+    } else {
+      currentKeluhan.add(keluhan);
+    }
+    state = state.copyWith(keluhan: currentKeluhan);
+  }
+
+  void setSickStartTime(DateTime sickStartTime) {
+    state = state.copyWith(sickStartTime: sickStartTime);
+  }
+
+  void setKlinikStatus(PeriksaKlinikStatus status) {
+    state = state.copyWith(periksaKlinikStatus: status);
+  }
+
+  Future<void> submitData() async {
+    state = state.copyWith(isSubmitting: true);
+    // Here you would typically send the data to a repository or API
+    // For example: await ref.read(healthRepositoryProvider).addHealthRecord(state);
+    
+    // Simulate network delay
+    await Future.delayed(const Duration(seconds: 1));
+
+    state = state.copyWith(isSubmitting: false);
+    reset();
+  }
+
+  void reset() {
+    state = PendataanKesehatanState();
+  }
+}
---
+++ lib/features/health_input/viewmodel/pendataan_kesehatan_provider.g.dart
@@ -0,0 +1,30 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'pendataan_kesehatan_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$pendataanKesehatanHash() =>
+    r'f4113846d8736ace74098b51191081e61ac34342';
+
+/// See also [PendataanKesehatan].
+@ProviderFor(PendataanKesehatan)
+final pendataanKesehatanProvider =
+    AutoDisposeNotifierProvider<
+      PendataanKesehatan,
+      PendataanKesehatanState
+    >.internal(
+      PendataanKesehatan.new,
+      name: r'pendataanKesehatanProvider',
+      debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+          ? null
+          : _$pendataanKesehatanHash,
+      dependencies: null,
+      allTransitiveDependencies: null,
+    );
+
+typedef _$PendataanKesehatan = AutoDisposeNotifier<PendataanKesehatanState>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
