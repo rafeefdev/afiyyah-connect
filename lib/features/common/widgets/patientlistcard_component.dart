30011ee0dd05fa171329c3eea6a7c8a3a139b641
rafeefdev <rafeef.dev03@gmail.com>
2025-07-12 10:07:44 +0700

    remove status badge

--- lib/features/common/widgets/patientlistcard_component.dart
+++ lib/features/common/widgets/patientlistcard_component.dart
@@ -1,27 +1,8 @@
 import 'package:afiyyah_connect/app/core/model/entities/santri.dart';
 import 'package:afiyyah_connect/app/themes/app_spacing.dart';
 import 'package:afiyyah_connect/features/common/utils/extensions.dart';
+import 'package:afiyyah_connect/features/common/utils/get_initials.dart';
 import 'package:flutter/material.dart';
-
-String getInitials(String inputString) {
-  if (inputString.isEmpty) {
-    return ''; // Mengembalikan string kosong jika inputnya kosong
-  }
-
-  List<String> words = inputString.split(
-    ' ',
-  ); // Memisahkan string berdasarkan spasi
-  String initials = '';
-
-  for (String word in words) {
-    if (word.isNotEmpty) {
-      initials += word[0]
-          .toUpperCase(); // Mengambil huruf pertama dan mengubahnya menjadi huruf kapital
-    }
-  }
-
-  return initials;
-}
 
 Widget listCardItem(
   BuildContext context, {
@@ -29,7 +10,7 @@ Widget listCardItem(
   String status = 'SEGERA RUJUK',
   bool isAlert = true,
   Color customStatusColor = Colors.red,
-  List<String> secondaryInfo = const <String>[],
+  required String info,
 }) {
   String santriInitial = getInitials(santri.name);
   //TODO : implement real kelas
@@ -45,22 +26,15 @@ Widget listCardItem(
           Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Container(
-                padding: EdgeInsets.all(2),
-                decoration: BoxDecoration(
-                  color: Colors.red.shade100,
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: Text(
-                  status,
-                  style: context.textTheme.labelSmall!.copyWith(
-                    color: Colors.red.shade900,
-                  ),
+              Text(santri.name, style: context.textTheme.titleSmall),
+              Text(
+                'Kelas 10A • Kamar A-15',
+                style: context.textTheme.bodyMedium!.copyWith(
+                  color: Colors.grey.shade700,
                 ),
               ),
-              Text(santri.name, style: context.textTheme.bodyLarge),
               Text(
-                'Kelas 10A • Kamar A-15 • Alergi Akut',
+                info,
                 style: context.textTheme.bodyMedium!.copyWith(
                   color: Colors.grey.shade700,
                 ),
