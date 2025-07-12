d87ad5477a77fd1911622584c75a67468cb415c4
rafeefdev <rafeef.dev03@gmail.com>
2025-07-12 10:05:47 +0700

    add page padding

--- lib/app/themes/app_spacing.dart
+++ lib/app/themes/app_spacing.dart
@@ -1,8 +1,15 @@
+import 'package:flutter/material.dart';
+
 class AppSpacing {
   static double xs = 4;
   static double s = 8;
   static double m = 12;
   static double l = 16;
   static double xl = 24;
-  static double xxl = 32; 
+  static double xxl = 32;
+
+  static EdgeInsets pagePadding = EdgeInsets.symmetric(
+    horizontal: AppSpacing.l,
+    vertical: AppSpacing.xl,
+  );
 }
