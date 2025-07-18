9f0ffbb85c686dad404ebcb8248185523ad4aac0
rafeefdev <rafeef.dev03@gmail.com>
2025-07-18 13:57:13 +0700

    display searched name results

--- lib/features/health_input/view/input_bottomsheet/step1_carisantri.dart
+++ lib/features/health_input/view/input_bottomsheet/step1_carisantri.dart
@@ -1,4 +1,6 @@
 import 'package:afiyyah_connect/app/themes/app_spacing.dart';
+import 'package:afiyyah_connect/features/common/utils/extension/extensions.dart';
+import 'package:afiyyah_connect/features/common/widgets/loadingindicator_component.dart';
 import 'package:afiyyah_connect/features/health_input/viewmodel/stepcontroller_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -10,23 +12,17 @@ class Step1CariSantri extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    // TODO : insert search state here (make sure this variable is assigned to future value)
+    bool isLoading = true;
+
     return Padding(
       padding: EdgeInsets.symmetric(horizontal: AppSpacing.s),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          TextField(
-            autofocus: true,
-            keyboardType: TextInputType.name,
-            decoration: InputDecoration(
-              hint: const Text('Ketik untuk mencari nama santri'),
-              label: const Text('Nama Santri'),
-              prefixIcon: const Icon(Icons.search_rounded),
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(10),
-              ),
-            ),
-          ),
+          _buildSearchNameField(isLoading),
+          // TODO : display search result. Replace index with result lenght
+          ..._buildNameResult(context, resultLength: 5),
           SizedBox(height: AppSpacing.l),
           Row(
             mainAxisAlignment: MainAxisAlignment.end,
@@ -49,4 +45,39 @@ class Step1CariSantri extends ConsumerWidget {
       ),
     );
   }
+
+  TextField _buildSearchNameField(bool isLoading) {
+    return TextField(
+      autofocus: true,
+      keyboardType: TextInputType.name,
+      decoration: InputDecoration(
+        hint: const Text('Ketik untuk mencari nama santri'),
+        label: const Text('Nama Santri'),
+        prefixIcon: const Icon(Icons.search_rounded),
+        suffix: LoadingIndicator(isLoading: isLoading, strokeWidth: 2.6),
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+      ),
+    );
+  }
+}
+
+List<Widget> _buildNameResult(
+  BuildContext context, {
+  required int resultLength,
+}) {
+  return List.generate(resultLength, (index) {
+    return InkWell(
+      onTap: () {
+        // TODO : add selected name to input data
+      },
+      child: ListTile(
+        title: Text(
+          'Fathan bin Fulan',
+          style: context.textTheme.bodyLarge!.copyWith(
+            color: Colors.grey.shade800,
+          ),
+        ),
+      ),
+    );
+  });
 }
