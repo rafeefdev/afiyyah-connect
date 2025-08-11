788c8c149a0938ff4796f43c2c2c4ace0d4a34e4
rafeefdev <rafeef.dev03@gmail.com>
2025-08-11 09:40:25 +0700

    add zero dummy data

--- lib/features/dashboard/model/dashboard_data.dart
+++ lib/features/dashboard/model/dashboard_data.dart
@@ -35,27 +35,85 @@ class DashboardData {
   });
 
   /// Factory method untuk generate dummy data dashboard
-  factory DashboardData.dummy() {
-    return DashboardData(
-      totalSakitPekanIni: 25,
-      persentasePerbandinganPekanLalu: 19.2, // misal naik 19.2% dari pekan lalu
-      kasusTerbanyak: "Flu",
-      jumlahKasusTerbanyak: 12,
-      butuhIstirahatMaskan: 9,
-      kasusBaruHariIni: {"Flu": 5, "Batuk": 3, "Demam": 2, "Pusing": 1},
-      kasusPerHari: [2, 5, 4, 6, 3, 2, 3], // 7 hari terakhir (Senin - Ahad)
-      kasusPerJenjang: [2, 4, 4, 6, 2, 8],
-      kasusPerAsrama: [24, 28],
-      pieJenisPenyakit: {"Flu": 12, "Batuk": 6, "Demam": 4, "Pusing": 3},
-      rujukanHariIni: [
-        Santri(id: '1', nama: 'Ahmad Fauzi', jenjang: 8, tahunMasuk: 2021),
-        Santri(id: '2', nama: 'Rizki Hidayat', jenjang: 9, tahunMasuk: 2020),
-      ],
-      sakitHariIni: [
-        Santri(id: '3', nama: 'Fajar Sidik', jenjang: 7, tahunMasuk: 2022),
-        Santri(id: '4', nama: 'Rafli Ananda', jenjang: 8, tahunMasuk: 2021),
-        Santri(id: '5', nama: 'Ilham Ramadhan', jenjang: 9, tahunMasuk: 2020),
-      ],
-    );
+  factory DashboardData.dummy({bool isEmptyData = false}) {
+    return isEmptyData
+        ? DashboardData(
+            totalSakitPekanIni: 0,
+            persentasePerbandinganPekanLalu:
+                0, // misal naik 19.2% dari pekan lalu
+            kasusTerbanyak: "Tidak ada",
+            jumlahKasusTerbanyak: 0,
+            butuhIstirahatMaskan: 0,
+            kasusBaruHariIni: {},
+            kasusPerHari: [
+              0,
+              0,
+              0,
+              0,
+              0,
+              0,
+              0,
+            ], // 7 hari terakhir (Senin - Ahad)
+            kasusPerJenjang: [],
+            kasusPerAsrama: [],
+            pieJenisPenyakit: {},
+            rujukanHariIni: [],
+            sakitHariIni: [],
+          )
+        : DashboardData(
+            totalSakitPekanIni: 25,
+            persentasePerbandinganPekanLalu:
+                19.2, // misal naik 19.2% dari pekan lalu
+            kasusTerbanyak: "Flu",
+            jumlahKasusTerbanyak: 12,
+            butuhIstirahatMaskan: 9,
+            kasusBaruHariIni: {"Flu": 5, "Batuk": 3, "Demam": 2, "Pusing": 1},
+            kasusPerHari: [
+              2,
+              5,
+              4,
+              6,
+              3,
+              2,
+              3,
+            ], // 7 hari terakhir (Senin - Ahad)
+            kasusPerJenjang: [2, 4, 4, 6, 2, 8],
+            kasusPerAsrama: [24, 28],
+            pieJenisPenyakit: {"Flu": 12, "Batuk": 6, "Demam": 4, "Pusing": 3},
+            rujukanHariIni: [
+              Santri(
+                id: '1',
+                nama: 'Ahmad Fauzi',
+                jenjang: 8,
+                tahunMasuk: 2021,
+              ),
+              Santri(
+                id: '2',
+                nama: 'Rizki Hidayat',
+                jenjang: 9,
+                tahunMasuk: 2020,
+              ),
+            ],
+            sakitHariIni: [
+              Santri(
+                id: '3',
+                nama: 'Fajar Sidik',
+                jenjang: 7,
+                tahunMasuk: 2022,
+              ),
+              Santri(
+                id: '4',
+                nama: 'Rafli Ananda',
+                jenjang: 8,
+                tahunMasuk: 2021,
+              ),
+              Santri(
+                id: '5',
+                nama: 'Ilham Ramadhan',
+                jenjang: 9,
+                tahunMasuk: 2020,
+              ),
+            ],
+          );
   }
 }
