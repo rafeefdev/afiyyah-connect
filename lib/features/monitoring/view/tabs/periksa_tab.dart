d749dca2cdb27a444acd0345ec3de44e78c371d3
rafeefdev <rafeef.dev03@gmail.com>
2025-08-12 14:48:46 +0700

    LIst variable for placing data and put DisplayZeroData widget inside Lis tview widget

--- lib/features/monitoring/view/tabs/periksa_tab.dart
+++ lib/features/monitoring/view/tabs/periksa_tab.dart
@@ -1,5 +1,5 @@
-
 import 'package:afiyyah_connect/app/core/model/entities/santri.dart';
+import 'package:afiyyah_connect/features/common/widgets/displayzerodata_component.dart';
 import 'package:afiyyah_connect/features/common/widgets/patientlistcard_component.dart';
 import 'package:afiyyah_connect/features/monitoring/view/tablegend_component.dart';
 import 'package:flutter/material.dart';
@@ -9,6 +9,9 @@ class PeriksaTab extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    //TODO : fetch to real data
+    List<Santri> data = List.filled(0, Santri.generateDummyData());
+
     List<MaterialColor> colors = [Colors.orange, Colors.blue];
     return ListView(
       children: [
@@ -18,14 +21,23 @@ class PeriksaTab extends StatelessWidget {
           labels: ['Belum\nPeriksa', 'Sudah\nPeriksa'],
         ),
         const SizedBox(height: 12),
-        //TODO : fetch to real data
-        ...List.generate(10, (index) {
-          return ListCardItem(
-            santri: Santri.generateDummyData(),
-            customNotchColor: index < 2 ? colors[0] : colors[1],
-            info: 'Batuk, pilek, panas',
-          );
-        }),
+        data.isEmpty
+            ? DisplayZeroData(
+                height: 480,
+                icon: Icons.health_and_safety_rounded,
+                message: 'Tidak ada santri sakit hari ini',
+              )
+            :
+              //TODO : fetch to real data
+              Column(
+                children: List.generate(10, (index) {
+                  return ListCardItem(
+                    santri: Santri.generateDummyData(),
+                    customNotchColor: index < 2 ? colors[0] : colors[1],
+                    info: 'Batuk, pilek, panas',
+                  );
+                }),
+              ),
       ],
     );
   }
--- lib/features/monitoring/view/tabs/rujukan_transportasi_tab.dart
+++ lib/features/monitoring/view/tabs/rujukan_transportasi_tab.dart
@@ -1,5 +1,5 @@
-
 import 'package:afiyyah_connect/app/core/model/entities/santri.dart';
+import 'package:afiyyah_connect/features/common/widgets/displayzerodata_component.dart';
 import 'package:afiyyah_connect/features/common/widgets/patientlistcard_component.dart';
 import 'package:afiyyah_connect/features/monitoring/view/tablegend_component.dart';
 import 'package:flutter/material.dart';
@@ -9,7 +9,10 @@ class RujukanNTransportasiTab extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    //TODO : fetch to real data
+    List<Santri> data = List.filled(0, Santri.generateDummyData());
     List<MaterialColor> colors = [Colors.orange, Colors.blue];
+
     return ListView(
       children: [
         tabLegend(
@@ -18,20 +21,23 @@ class RujukanNTransportasiTab extends StatelessWidget {
           labels: ['Perlu\nDiantar', 'Sudah\nDiantar'],
         ),
         const SizedBox(height: 12),
-        //TODO : fetch to real data
-        ...List.generate(3, (index) {
-          return ListCardItem(
-            santri: Santri.generateDummyData(),
-            additionalTiles: [
-              ListTile(
-                title: Text('RS / Klinik Rujukan'),
-                leading: Icon(Icons.health_and_safety_rounded),
+        data.isEmpty
+            ? DisplayZeroData(
+                height: 480,
+                icon: Icons.check_circle_outline_rounded,
+                message: 'Tidak ada santri yang dirujuk',
+              )
+            :
+              //TODO : fetch to real data
+              Column(
+                children: List.generate(10, (index) {
+                  return ListCardItem(
+                    santri: Santri.generateDummyData(),
+                    customNotchColor: index < 2 ? colors[0] : colors[1],
+                    info: 'Batuk, pilek, panas',
+                  );
+                }),
               ),
-            ],
-            info: 'Batuk, pilek, panas',
-            customNotchColor: index < 2 ? colors[0] : colors[1],
-          );
-        }),
       ],
     );
   }
