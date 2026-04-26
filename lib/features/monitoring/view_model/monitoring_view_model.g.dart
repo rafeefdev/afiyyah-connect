8637f4fe781b7ebefa99ee78eccb4d43d4576a49
feefDev <rafeef.dev03@gmail.com>
2026-04-26 11:06:13 +0700

    feat: initialize viewmodels and fix repository part declarations
    - Add dashboard, medical_history, and monitoring view_model directories
    - Add missing part declaration in PendataanKesehatanRepository
    - Add generated repository code

---
+++ lib/features/monitoring/view_model/monitoring_view_model.g.dart
@@ -0,0 +1,84 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'monitoring_view_model.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$monitoringViewModelHash() =>
+    r'9550d7a7bb16ac07036b537a77e8086aa3ed6366';
+
+/// See also [MonitoringViewModel].
+@ProviderFor(MonitoringViewModel)
+final monitoringViewModelProvider =
+    AutoDisposeAsyncNotifierProvider<
+      MonitoringViewModel,
+      MonitoringData
+    >.internal(
+      MonitoringViewModel.new,
+      name: r'monitoringViewModelProvider',
+      debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+          ? null
+          : _$monitoringViewModelHash,
+      dependencies: null,
+      allTransitiveDependencies: null,
+    );
+
+typedef _$MonitoringViewModel = AutoDisposeAsyncNotifier<MonitoringData>;
+String _$periksaListHash() => r'25c994c142e59922b3e8863cf033e3c337ea3d5e';
+
+/// See also [PeriksaList].
+@ProviderFor(PeriksaList)
+final periksaListProvider =
+    AutoDisposeAsyncNotifierProvider<
+      PeriksaList,
+      List<PendataanKesehatanModel>
+    >.internal(
+      PeriksaList.new,
+      name: r'periksaListProvider',
+      debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+          ? null
+          : _$periksaListHash,
+      dependencies: null,
+      allTransitiveDependencies: null,
+    );
+
+typedef _$PeriksaList = AutoDisposeAsyncNotifier<List<PendataanKesehatanModel>>;
+String _$arahanListHash() => r'3aaafdfffdcc885bd1b5e6e3d9375596c7ddd808';
+
+/// See also [ArahanList].
+@ProviderFor(ArahanList)
+final arahanListProvider =
+    AutoDisposeAsyncNotifierProvider<
+      ArahanList,
+      List<KunjunganKlinikModel>
+    >.internal(
+      ArahanList.new,
+      name: r'arahanListProvider',
+      debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+          ? null
+          : _$arahanListHash,
+      dependencies: null,
+      allTransitiveDependencies: null,
+    );
+
+typedef _$ArahanList = AutoDisposeAsyncNotifier<List<KunjunganKlinikModel>>;
+String _$rujukanListHash() => r'23b03c9edc4a995420606b04030a8b13128a7e54';
+
+/// See also [RujukanList].
+@ProviderFor(RujukanList)
+final rujukanListProvider =
+    AutoDisposeAsyncNotifierProvider<RujukanList, List<RujukanModel>>.internal(
+      RujukanList.new,
+      name: r'rujukanListProvider',
+      debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+          ? null
+          : _$rujukanListHash,
+      dependencies: null,
+      allTransitiveDependencies: null,
+    );
+
+typedef _$RujukanList = AutoDisposeAsyncNotifier<List<RujukanModel>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
